65f4b80d164b60809a244050b8806ae5c08162f5
misho92 <matachev92@yahoo.co.uk>
2014-07-12 10:37:05 +0200

    Folder changes

---
+++ Todo/todo.py
@@ -0,0 +1,50 @@
+from __future__ import with_statement
+from flask import Flask, send_file, make_response
+from todoView import todo,todoDelete,mark,account,signin,myinfo
+
+# set flask app options
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar("FLASKR_SETTINGS",silent=True)
+
+# add url rules with the corresponding view and method
+app.add_url_rule("/todo",   view_func=todo.as_view("todo"), methods=["GET","POST"])
+app.add_url_rule("/todo/<string:task>/<int:user>", view_func=todoDelete.as_view("todoDelete"), methods=["DELETE", "PUT"])
+app.add_url_rule("/mark/<string:action>/<int:user>", view_func=mark.as_view("mark"), methods=["POST"])
+app.add_url_rule("/account", view_func=account.as_view("account"), methods=["POST"])
+app.add_url_rule("/signin", view_func=signin.as_view("signin"), methods=["POST"])
+app.add_url_rule("/myaccount", view_func=myinfo.as_view("myinfo"), methods=["GET","PUT"])
+
+# make_response(open("index.html").read()) for no caching
+
+@app.route("/todos")
+def todos():
+    return send_file("todo.html")
+
+@app.route("/app.js")
+def js():
+    return send_file("static/app.js")
+
+@app.route("/styles.css")
+def css():
+    return send_file("static/styles.css")
+
+@app.route("/")
+def index():
+    return send_file("index.html")
+
+@app.route("/signin")
+def sigin():
+    return send_file("index.html")
+
+@app.route("/signup")
+def signup():
+    return send_file("signup.html")
+
+@app.route("/myinfo")
+def myaccount():
+    return send_file("myaccount.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
